36f584fe714e4e18e716af15db97e77762098ce1
IsaacRWI <isaacngrwi@gmail.com>
2025-11-11 22:23:10 +0000

    Added bcrypt and csrf to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_wtf import CSRFProtect
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+csrf = CSRFProtect()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    csrf.init_app(app)
+    bcrypt.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
