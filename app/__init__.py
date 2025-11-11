2d08ec46509446e664667fd448ba7da2c5118c5c
IsaacRWI <isaacngrwi@gmail.com>
2025-11-11 22:27:39 +0000

    Added login manager to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_wtf import CSRFProtect
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
 bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +17,10 @@ def create_app():
     db.init_app(app)
     csrf.init_app(app)
     bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    login_manager.login_view = "main.login"
+    login_manager.login_message_category = "info"
 
     from .routes import main
     app.register_blueprint(main)
