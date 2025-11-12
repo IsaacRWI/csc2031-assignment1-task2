4081474a80b4232bd8ef62a2ed614fdd97581eb3
IsaacRWI <isaacngrwi@gmail.com>
2025-11-12 20:01:45 +0000

    Implemented logout functionality and session regeneration

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session
 from app import login_manager
 from app.forms import LoginForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user,login_required
+from uuid import uuid4
 
 main = Blueprint('main', __name__)
 
@@ -15,6 +16,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user and user.check_password(form.password.data):
             login_user(user)
+            regenerate_session()
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('main.dashboard'))
         else:
@@ -27,8 +29,17 @@ def dashboard():
 
 @main.route('/logout')
 def logout():
+    logout_user()
     return redirect(url_for('main.login'))
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+def regenerate_session():
+    """had to ask ai for help with this function"""
+    session.new = dict(session)
+    session.clear()
+    session.update(session.new)
+    session["csrf_token"] = uuid4().hex
+    # print("session regenerate")
