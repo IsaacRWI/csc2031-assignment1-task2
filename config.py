68fe2d3f70183c9dafab3204197f3f85b0426c27
IsaacRWI <isaacngrwi@gmail.com>
2025-11-12 20:34:48 +0000

    Implemented secret key in .env

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
+import os
+from dotenv import load_dotenv
+
 class Config:
-    SECRET_KEY = 'devkey123'
+    load_dotenv()
+    SECRET_KEY = os.getenv('SECRET_KEY')  # SECRET_KEY = 'devkey123' in .env
     SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     TESTING = True
+
+
+# config_test = Config()
+# print(config_test.SECRET_KEY)
