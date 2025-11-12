6f19d9b725dd8047b8c071a880011b3a1fe2d386
IsaacRWI <isaacngrwi@gmail.com>
2025-11-12 21:21:06 +0000

    Added new forms for one time code and mfa set up forms

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, length
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('login')
+
+# had to ask ai for pointers on how to do this
+class TOTPForm(FlaskForm):
+    token = StringField('authentication code', validators=[DataRequired(), length(min=6, max=6)])
+    submit = SubmitField('verify')
+
+class SetUpMFAForm(FlaskForm):
+    token = StringField('authentication code', validators=[DataRequired(), length(min=6, max=6)])
+    submit = SubmitField('enable mfa')
