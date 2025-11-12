d554152aabde78bbb2fce580f2a5e77380069e7d
IsaacRWI <isaacngrwi@gmail.com>
2025-11-12 00:45:44 +0000

    Added methods to hash and compare hashed passwords

--- app/models.py
+++ app/models.py
@@ -1,7 +1,14 @@
 from . import db
 from flask_login import UserMixin
+from flask import bcrypt
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+
+    def hash_password(self, text_password):
+        self.password = bcrypt.generate_password_hash(text_password).decode('utf-8')
+
+    def check_password(self, text_password):
+        return bcrypt.check_password_hash(self.password, text_password)
