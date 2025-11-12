5682cd5e7234fa6736083fd7a1cf338b74e6bb39
IsaacRWI <isaacngrwi@gmail.com>
2025-11-12 23:56:04 +0000

    Added column to track login attempts and timeout for users - Added method to check if user is on timeout

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db, bcrypt
 from flask_login import UserMixin
 import pyotp
+from datetime import datetime, timezone
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,6 +9,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(128), nullable=False)
     totp_secret = db.Column(db.String(16), nullable=False, default="")  # had to ask ai to help find this
     mfa_enabled = db.Column(db.Boolean, nullable=False, default=False)   # i forgot set default values for non-nullable fields
+    attempts = db.Column(db.Integer, nullable=False, default=0)
+    lockout_until = db.Column(db.DateTime, nullable=True)
 
     def hash_password(self, text_password):
         self.password = bcrypt.generate_password_hash(text_password).decode('utf-8')
@@ -24,3 +27,8 @@ class User(db.Model, UserMixin):
             return False
         totp = pyotp.TOTP(self.totp_secret)
         return totp.verify(token)
+
+    def is_locked(self):
+        if self.lockout_until and self.lockout_until > datetime.now(timezone.utc):
+            return True
+        else: return False
