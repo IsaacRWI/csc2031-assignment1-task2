96f220485b27d5f0268c3927e6156c1b974cc717
IsaacRWI <isaacngrwi@gmail.com>
2025-11-12 21:53:13 +0000

    Implemented form to enable mfa

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash, session
-from app import login_manager
-from app.forms import LoginForm
+from app import login_manager, db
+from app.forms import LoginForm,TOTPForm,SetUpMFAForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user,login_required
 from uuid import uuid4
+import pyotp
+import qrcode
+import io
+import base64
 
 main = Blueprint('main', __name__)
 
@@ -33,6 +37,37 @@ def dashboard():
 def logout():
     logout_user()
     return redirect(url_for('main.login'))
+
+@main.route("/mfa_setup", method=["GET", "POST"])
+@login_required
+def mfa_setup():
+    user = current_user
+    if user.mfa_enabled:
+        flash("MFA is already enabled on this account", "info")
+        return redirect(url_for("main.dashboard"))
+    if not user.totp_secret:
+        user.totp_secret = pyotp.random_base32()
+        db.session.commit()
+
+    uri =user.get_totp_uri()
+    qr = qrcode.make(uri)
+    buf = io.BytesIO()
+    qr.save(buf, format="PNG")
+    qr_b64 = base64.b64encode(buf.getvalue()).decode("ascii")
+
+    form = SetUpMFAForm()
+    if form.validate_on_submit():
+        token = form.token.data
+        if user.verify_totp(token):
+            user.mfa_enabled = True
+            db.session.commit()
+            flash("MFA has been enabled for your account.", "info")
+            return redirect(url_for('main.dashboard'))
+        else:
+            flash("Invalid authentication code. Try again.", "danger")
+
+    return render_template('mfa_setup.html', form=form, qr_code=qr_b64, secret=user.totp_secret)
+
 
 @login_manager.user_loader
 def load_user(user_id):
